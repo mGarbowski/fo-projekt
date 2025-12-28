42b760ab4cb4aded6fa639dc106b5acea983041f
mgarbowski <mikolaj.garbowski@gmail.com>
2025-12-28 15:11:58 +0100

    feat: move training code to library

--- supernova/dataset.py
+++ supernova/dataset.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 import torch
 from torch.nn.utils.rnn import pad_sequence
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 N_BANDS = 6
 
@@ -176,4 +176,71 @@ def supernova_collate_fn(batch):
         "sequences": band_sequences,
         "lengths": lengths,
         "labels": labels,
+    }
+
+
+def get_dataset_split(
+    dataset_path: str,
+    val_split: float,
+    test_split: float,
+    random_seed: int = 42,
+) -> dict[str, Dataset]:
+    assert 0 < val_split < 1
+    assert 0 < test_split < 1
+    assert val_split + test_split < 1
+
+    dataset = SupernovaDataset(dataset_path)
+
+    total_size = len(SupernovaDataset(dataset_path))
+    test_size = int(total_size * test_split)
+    val_size = int(total_size * val_split)
+    train_size = total_size - val_size - test_size
+
+    generator = torch.Generator().manual_seed(random_seed)
+    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(
+        dataset,
+        [train_size, val_size, test_size],
+        generator=generator,
+    )
+
+    return {
+        "train": train_dataset,
+        "val": val_dataset,
+        "test": test_dataset,
+    }
+
+
+def get_data_loaders(
+    datasets: dict[str, Dataset],
+    batch_size: int,
+    num_workers: int = 4,
+) -> dict[str, DataLoader]:
+    train_loader = DataLoader(
+        datasets["train"],
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        collate_fn=supernova_collate_fn,
+    )
+
+    val_loader = DataLoader(
+        datasets["val"],
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        collate_fn=supernova_collate_fn,
+    )
+
+    test_loader = DataLoader(
+        datasets["test"],
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        collate_fn=supernova_collate_fn,
+    )
+
+    return {
+        "train": train_loader,
+        "val": val_loader,
+        "test": test_loader,
     }
